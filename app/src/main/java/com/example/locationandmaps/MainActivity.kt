784313e2bf7f29925e2267a3038d0d6cba2855ef
Jujare Vinayak <jujare.vinayak@mercedes.com>
2024-04-21 20:21:38 +0530

    Get Last Location And GeoCoder

---
+++ app/src/main/java/com/example/locationandmaps/MainActivity.kt
@@ -0,0 +1,396 @@
+package com.example.locationandmaps
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.example.locationandmaps.ui.theme.LocationAndMapsTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.util.Locale
+
+@Suppress("DEPRECATION")
+class MainActivity : ComponentActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            var locationPermissionsGranted by remember {
+                mutableStateOf(
+                    areLocationPermissionsAlreadyGranted()
+                )
+            }
+            var shouldShowPermissionRationale by remember {
+                mutableStateOf(
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                )
+            }
+
+            var shouldDirectUserToApplicationSettings by remember {
+                mutableStateOf(false)
+            }
+
+            var currentPermissionsStatus by remember {
+                mutableStateOf(
+                    decideCurrentPermissionStatus(
+                        locationPermissionsGranted,
+                        shouldShowPermissionRationale
+                    )
+                )
+            }
+
+            var locationText by remember {
+                mutableStateOf("")
+            }
+
+            var readableLocation by remember {
+                mutableStateOf("")
+            }
+
+            val locationPermissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+
+            val locationPermissionLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestMultiplePermissions(),
+                onResult = { permissions ->
+                    locationPermissionsGranted =
+                        permissions.values.reduce { acc, isPermissionGranted ->
+                            acc && isPermissionGranted
+                        }
+
+                    if (!locationPermissionsGranted) {
+                        shouldShowPermissionRationale =
+                            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    }
+                    shouldDirectUserToApplicationSettings =
+                        !shouldShowPermissionRationale && !locationPermissionsGranted
+                    currentPermissionsStatus = decideCurrentPermissionStatus(
+                        locationPermissionsGranted,
+                        shouldShowPermissionRationale
+                    )
+                    if (locationPermissionsGranted) {
+                        getLastUserLocation(
+                            onGetLastLocationSuccess = {
+                                locationText = "LATITUDE: ${it.first}, LONGITUDE: ${it.second}"
+                                readableLocation = getReadableLocation(it.first, it.second, applicationContext)
+                            },
+                            onGetLastLocationFailed = { exception ->
+                                locationText =
+                                    exception.localizedMessage ?: "Error Getting Last Location"
+                            },
+                            onGetLastLocationIsNull = {
+                                // Attempt to get the current user location
+                                getCurrentLocation(
+                                    onGetCurrentLocationSuccess = {
+                                        locationText =
+                                            "LATITUDE: ${it.first}, LONGITUDE: ${it.second}"
+                                    },
+                                    onGetCurrentLocationFailed = {
+                                        locationText =
+                                            it.localizedMessage
+                                                ?: "Error Getting Current Location"
+                                    }
+                                )
+                            }
+                        )
+                    }
+                })
+
+            val lifecycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(key1 = lifecycleOwner, effect = {
+                val observer = LifecycleEventObserver { _, event ->
+                    if (event == Lifecycle.Event.ON_START &&
+                        !locationPermissionsGranted &&
+                        !shouldShowPermissionRationale
+                    ) {
+                        locationPermissionLauncher.launch(locationPermissions)
+                    }
+                }
+                lifecycleOwner.lifecycle.addObserver(observer)
+                onDispose {
+                    lifecycleOwner.lifecycle.removeObserver(observer)
+                }
+            }
+            )
+
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+
+            LocationAndMapsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }) { contentPadding ->
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.height(35.dp))
+                            Text(
+                                modifier = Modifier
+                                    .padding(contentPadding)
+                                    .fillMaxWidth(),
+                                text = "Current Permission Status: $currentPermissionsStatus",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(35.dp))
+                            Text(
+                                modifier = Modifier
+                                    .padding(contentPadding)
+                                    .fillMaxWidth(),
+                                text = "Location: $locationText",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(35.dp))
+                            Text(
+                                modifier = Modifier
+                                    .padding(contentPadding)
+                                    .fillMaxWidth(),
+                                text = "Human Readable Location: $readableLocation",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                    if (locationPermissionsGranted) {
+                        getLastUserLocation(
+                            onGetLastLocationSuccess = {
+                                locationText = "LATITUDE: ${it.first}, LONGITUDE: ${it.second}"
+                                readableLocation = getReadableLocation(it.first, it.second, applicationContext)
+                            },
+                            onGetLastLocationFailed = { exception ->
+                                locationText =
+                                    exception.localizedMessage ?: "Error Getting Last Location"
+                            },
+                            onGetLastLocationIsNull = {
+                                // Attempt to get the current user location
+                                getCurrentLocation(
+                                    onGetCurrentLocationSuccess = {
+                                        locationText =
+                                            "LATITUDE: ${it.first}, LONGITUDE: ${it.second}"
+                                    },
+                                    onGetCurrentLocationFailed = {
+                                        locationText =
+                                            it.localizedMessage
+                                                ?: "Error Getting Current Location"
+                                    }
+                                )
+                            }
+                        )
+                    }
+                    if (shouldShowPermissionRationale) {
+                        LaunchedEffect(Unit) {
+                            scope.launch {
+                                val userAction = snackbarHostState.showSnackbar(
+                                    message = "Please authorize location permissions",
+                                    actionLabel = "Approve",
+                                    duration = SnackbarDuration.Indefinite,
+                                    withDismissAction = true
+                                )
+                                when (userAction) {
+                                    SnackbarResult.ActionPerformed -> {
+                                        shouldShowPermissionRationale = false
+                                        locationPermissionLauncher.launch(locationPermissions)
+                                    }
+
+                                    SnackbarResult.Dismissed -> {
+                                        shouldShowPermissionRationale = false
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (shouldDirectUserToApplicationSettings) {
+                        openApplicationSettings()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun areLocationPermissionsAlreadyGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun openApplicationSettings() {
+        Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        ).also {
+            startActivity(it)
+        }
+    }
+
+    private fun decideCurrentPermissionStatus(
+        locationPermissionsGranted: Boolean,
+        shouldShowPermissionRationale: Boolean
+    ): String {
+        return if (locationPermissionsGranted) "Granted"
+        else if (shouldShowPermissionRationale) "Rejected"
+        else "Denied"
+    }
+
+    /**
+     * Retrieves the last known user location asynchronously.
+     *
+     * @param onGetLastLocationSuccess Callback function invoked when the location is successfully retrieved.
+     *        It provides a Pair representing latitude and longitude.
+     * @param onGetLastLocationFailed Callback function invoked when an error occurs while retrieving the location.
+     *        It provides the Exception that occurred.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getLastUserLocation(
+        onGetLastLocationSuccess: (Pair<Double, Double>) -> Unit,
+        onGetLastLocationFailed: (Exception) -> Unit,
+        onGetLastLocationIsNull: () -> Unit
+    ) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // Check if location permissions are granted
+        if (areLocationPermissionsAlreadyGranted()) {
+            // Retrieve the last known location
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location ->
+                    location?.let {
+                        // If location is not null, invoke the success callback with latitude and longitude
+                        onGetLastLocationSuccess(Pair(it.latitude, it.longitude))
+                    }?.run {
+                        onGetLastLocationIsNull()
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // If an error occurs, invoke the failure callback with the exception
+                    onGetLastLocationFailed(exception)
+                }
+        }
+    }
+
+    /**
+     * Retrieves the current user location asynchronously.
+     *
+     * @param onGetCurrentLocationSuccess Callback function invoked when the current location is successfully retrieved.
+     *        It provides a Pair representing latitude and longitude.
+     * @param onGetCurrentLocationFailed Callback function invoked when an error occurs while retrieving the current location.
+     *        It provides the Exception that occurred.
+     * @param priority Indicates the desired accuracy of the location retrieval. Default is high accuracy.
+     *        If set to false, it uses balanced power accuracy.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(
+        onGetCurrentLocationSuccess: (Pair<Double, Double>) -> Unit,
+        onGetCurrentLocationFailed: (Exception) -> Unit,
+        priority: Boolean = true
+    ) {
+        // Determine the accuracy priority based on the 'priority' parameter
+        val accuracy = if (priority) Priority.PRIORITY_HIGH_ACCURACY
+        else Priority.PRIORITY_BALANCED_POWER_ACCURACY
+
+        // Check if location permissions are granted
+        if (areLocationPermissionsAlreadyGranted()) {
+            // Retrieve the current location asynchronously
+            fusedLocationProviderClient.getCurrentLocation(
+                accuracy, CancellationTokenSource().token,
+            ).addOnSuccessListener { location ->
+                location?.let {
+                    // If location is not null, invoke the success callback with latitude and longitude
+                    onGetCurrentLocationSuccess(Pair(it.latitude, it.longitude))
+                }?.run {
+                    //Location null do something
+                }
+            }.addOnFailureListener { exception ->
+                // If an error occurs, invoke the failure callback with the exception
+                onGetCurrentLocationFailed(exception)
+            }
+        }
+    }
+
+    private fun getReadableLocation(latitude: Double, longitude: Double, context: Context): String {
+        var addressText = ""
+        val geocoder = Geocoder(context, Locale.getDefault())
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                geocoder.getFromLocation(latitude, longitude, 1) { addresses ->
+                    if (addresses.isNotEmpty()) {
+                        val address = addresses[0]
+                        addressText = "${address.getAddressLine(0)}, ${address.locality}"
+                        // Use the addressText in your app
+                        Log.d("geolocation", addressText)
+                    }
+                }
+            } else {
+                val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+                if (addresses?.isNotEmpty() == true) {
+                    val address = addresses[0]
+                    addressText = "${address.getAddressLine(0)}, ${address.locality}"
+                    // Use the addressText in your app
+                    Log.d("geolocation", addressText)
+                }
+            }
+        } catch (e: IOException) {
+            Log.d("geolocation", e.message.toString())
+        }
+
+        return addressText
+
+    }
+}
